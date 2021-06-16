3c6cbf7ac25b3b1862f25c21d74373c64fd94d99
刘港 <liugang@liugangdeMacBook-Pro.local>
2021-06-16 23:41:15 +0800

    fitst

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,13 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="item">
+      000
+      <h1>
+        xxx
+        <span>222</span>
+      </h1>
+    </div>
   </div>
 </template>
 
@@ -17,6 +24,13 @@ export default {
 </script>
 
 <style lang="scss">
+$colorRed: red;
+.item{
+  color: red;
+  h1{
+    color:blue
+  }
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
